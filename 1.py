2e846aca0aa58829cd0cd4d11addf4005ab32335
Stas-sts34 <stas_sts@mail.ru>
2022-03-18 21:56:27 +0300

    task

--- 1.py
+++ 1.py
@@ -1,37 +1,42 @@
-import pprint
 
-
-# n = int(input('сколько позиций надо внести?'))
+n = int(input('сколько позиций надо внести?'))
 product = []
 dict = {}
 
-# for i, _ in enumerate(range(n), 1):
-#     name = input('введите название товара')
-#     price = int(input('введите цену товара'))
-#     number = int(input('введите кол-во товара'))
-#     unit = input('введите ед.измерения')
-#     globals()[f'dict{i}'] = {}
-#     globals()[f'dict{i}']['название'] = name
-#     globals()[f'dict{i}']['цена'] = price
-#     globals()[f'dict{i}']['количество'] = number
-#     globals()[f'dict{i}']['eд'] = unit
-#     tuple = i, globals()[f'dict{i}']
-#     product.append(tuple)
+for i, _ in enumerate(range(n), 1):
+    name = input('введите название товара')
+    price = int(input('введите цену товара'))
+    number = int(input('введите кол-во товара'))
+    unit = input('введите ед.измерения')
+    globals()[f'dict{i}'] = {}
+    globals()[f'dict{i}']['название'] = name
+    globals()[f'dict{i}']['цена'] = price
+    globals()[f'dict{i}']['количество'] = number
+    globals()[f'dict{i}']['eд'] = unit
+    tuple = i, globals()[f'dict{i}']
+    product.append(tuple)
 
-product = [(1, {'название': 'q', 'цена': 2, 'количество': 2, 'eд': 'q'}), (2, {'название': 'w', 'цена': 2, 'количество': 2, 'eд': 'w'})]
-total_dict = {name for name in product}
-total_list = []
+
+total_dict = {}
+total_name = []
+total_price = []
+total_number = []
+total_unit = []
 for i in product:
-    # total_dict['название'] = [name for name in i[1]['название']]
-    total_list.append(i[1]['название'])
+    total_name.append(i[1]['название'])
+    total_price.append(i[1]['цена'])
+    total_number.append(i[1]['количество'])
+    total_unit.append(i[1]['eд'])
 
-print(total_dict, total_list)
-    # for j in i[1]:
-    #     print(j)
+total_dict['название'] = total_name
+total_dict['цена'] = total_price
+total_dict['количество'] = total_number
+total_dict['ед'] = total_unit
+
+print(total_dict)
 
 
 
 
-# pprint.pprint(product)
-# print(product)
-# print(product[0] is product[1])
+
+
