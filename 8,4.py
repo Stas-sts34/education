b2c4916e8ec1b96caf1aa281dfac565db84f506e
Stas-sts34 <stas_sts@mail.ru>
2022-04-15 22:14:01 +0300

    Исправил, улучшил. лекция 8: задание 4-6

--- 8,4.py
+++ 8,4.py
@@ -14,7 +14,6 @@
  нельзя использовать строковый тип данных.
 Подсказка: постарайтесь реализовать в проекте «Склад оргтехники» максимум возможностей,
 изученных на уроках по ООП.
-
 '''
 
 
@@ -23,39 +22,35 @@ class Warehouse:
     склад хранения оргтехники
     '''
     def __init__(self, name):
-        self.name = name
-        self.warehouse = {                      # словарь для данных склада
-            'склад': self.name
+        self.warehouse_name = name
+
+        self._warehouse = {                      # словарь для данных склада
+
         }
 
-    def add_warehouse(self, product, count):            # метод добавление/оприходывание товара на склад
-        if self.warehouse.get(product['устройство']) == None:
-            self.warehouse[product['устройство']] = int(count)
-            return self.warehouse
+    def add_product(self, product, count):            # метод добавление/оприходывание товара на склад
+        if self._warehouse.get(product.name) == None:
+            self._warehouse[product.name] = int(count)
+            return self._warehouse
         else:
-            self.warehouse[product['устройство']] += int(count)
-            return self.warehouse
+            self._warehouse[product.name] += int(count)
+            return self._warehouse
 
     def give_from_warehouse(self, other, product, count):       # метод перемещение товаров со склада на склад
         try:
-            result = self.warehouse[product['устройство']] - int(count)
+            result = self._warehouse[product.name] - int(count)
             if result < 0:
                 print('невозможно выгрузить больше, чем есть на складе')
             else:
-                self.warehouse[product['устройство']] = result
-                if other.warehouse.get(product['устройство']) == None:
-                    other.warehouse[product['устройство']] = int(count)
-                    return other.warehouse
-                else:
-                    other.warehouse[product['устройство']] += int(count)
-                    return other.warehouse
+                self._warehouse[product.name] = result
+                other.add_product(product, count)
 
         except TypeError:
             print(' не правильно ввели данные')
 
 
     def get_warehouse(self):
-        return self.warehouse
+        return self.__dict__            # можно ли так возвращать словарь?
 
 
 
@@ -64,59 +59,72 @@ class Office_equipment:
     def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.item = {'устройство': self.name,
-                     'цена': self.price,
-                     }
+        # self.item = {'устройство': self.name,
+        #              'цена': self.price,
+        #              }
+
+    def __getitem__(self, item):            # для упрощения добавления объектов на склад
+        return self.__dict__[item]
+
+    def __setitem__(self, key, value):      # можно и так устанавливать цену
+        self.__dict__[key] = value
+
+    def __str__(self):
+        return f'{self.__dict__}'
 
     @property
-    def get_price(self):
-        return self.item
+    def set_price(self):
+        return self.price
 
-    @get_price.setter
-    def get_price(self, price):
-        self.item['цена'] = price
-
-    def get_item(self):
-        return self.item
+    @set_price.setter
+    def set_price(self, price):
+        self.price = price
 
 
 class Printer(Office_equipment):
     def printer_speed(self, speed):
         self.speed = speed
-        self.item['скорость печати'] = self.speed
-        return self.item
+        # self.item['скорость печати'] = self.speed
+        return self.speed
 
 
 class Scanner(Office_equipment):
     def scanner_speed(self, speed):
         self.speed = speed
-        self.item['скорость сканирования'] = self.speed
-        return self.item
+        # self.item['скорость сканирования'] = self.speed
+        return self.speed
 
 
 class Xerox(Office_equipment):
     def xerox_speed(self, speed):
         self.speed = speed
-        self.item['скорость копирования'] = self.speed
-        return self.item
+        # self.item['скорость копирования'] = self.speed
+        return self.speed
 
 
 p = Printer('hp', 125)
 s = Scanner('canon', 300)
 p.printer_speed(5)      # устанавливаем свойство скорости печати
-p.get_price=100         # устанавливаем цену
+p.set_price = 100         # устанавливаем цену
+p.price = 99    # по другому устанавливаем цену ( как вариант так можно? )
 x=Xerox('sams', 200)
-print('товар ',p.get_item())
+print('товар ', p, s, x)
 
 
 warehouse = Warehouse('основной')       # создаем склад 1
 skl = Warehouse('коммерч')              # создаем склад 2
-warehouse.add_warehouse(p.get_item(), 12)   # добавляем товар и кол-во
-warehouse.add_warehouse(s.get_item(), 10)   # добавляем товар и кол-во
-skl.add_warehouse(x.get_item(), 5)          # перемещаем на другой склад
+# print(p)
+
+warehouse.add_product(p, 12)   # добавляем товар и кол-во
+warehouse.add_product(s, 5)
+warehouse.add_product(x, 1)
+# print(warehouse.__dict__)
+warehouse.add_product(s, 10)   # добавляем товар и кол-во
+print(warehouse.get_warehouse())
+skl.add_product(x, 5)          # перемещаем на другой склад
 print('склады до', warehouse.get_warehouse(), skl.get_warehouse())
-warehouse.give_from_warehouse(skl, p.get_item(), 3)     # еще перемещаем
-warehouse.give_from_warehouse(skl, s.get_item(), 5)     # еще перемещаем
+warehouse.give_from_warehouse(skl, p, 3)     # еще перемещаем
+warehouse.give_from_warehouse(skl, s, 5)     # еще перемещаем
 print('склады после', warehouse.get_warehouse(), skl.get_warehouse())
-skl.give_from_warehouse(warehouse, s.get_item(),2)      # перемещаем обратно на основной склад
+skl.give_from_warehouse(warehouse, s,2)      # перемещаем обратно на основной склад
 print('склады end', warehouse.get_warehouse(), skl.get_warehouse())
